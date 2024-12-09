d3f670d140d4d385de139fb0513a447b80311b2d
evwalz <evamaria.walz@gmx.de>
2024-12-09 11:45:05 +0100

    download era5 scripts

--- scripts/download_data/helpers.py
+++ scripts/download_data/helpers.py
@@ -18,26 +18,26 @@ def con_grid(input_file, output_file, target_grid_file):
 
 
 def get_shear(data_dir):
-    u925_file = data_dir + 'era5_10m_u_component_of_wind_925.nc'
-    u600_file = data_dir + 'era5_10m_u_component_of_wind_600.nc'
-    v925_file = data_dir + 'era5_10m_v_component_of_wind_925.nc'
-    v600_file = data_dir + 'era5_10m_v_component_of_wind_600.nc'
+    u925_file = xr.open_dataset(data_dir + 'era5_u_component_of_wind_925.nc')
+    u600_file = xr.open_dataset(data_dir + 'era5_u_component_of_wind_600.nc')
+    v925_file = xr.open_dataset(data_dir + 'era5_v_component_of_wind_925.nc')
+    v600_file = xr.open_dataset(data_dir + 'era5_v_component_of_wind_600.nc')
     u925 = sel_grid(u925_file)
     u600 = sel_grid(u600_file)
     v925 = sel_grid(v925_file)
     v600 = sel_grid(v600_file)
     z = 925 - 600
-    diff_u = u925 - u600
-    diff_v = v925 - v600
+    diff_u = u925.squeeze().u - u600.squeeze().u
+    diff_v = v925.squeeze().v - v600.squeeze().v
     diff_u2 = diff_u ** 2
     diff_v2 = diff_v ** 2
-    diff_mag = (diff_u2['u'] + diff_v2['v'])**(0.5)
+    diff_mag = (diff_u2 + diff_v2)**(0.5)
     shear = diff_mag / z
     # Create a new DataArray for shear
     shear_da = xr.DataArray(
         data=shear, 
-        dims=u925.dims,  # Use the same dimensions as diff_mag
-        coords=u925.coords,  # Copy coordinates from diff_mag
+        dims=u925.squeeze().dims,  # Use the same dimensions as diff_mag
+        coords=u925.squeeze().coords,  # Copy coordinates from diff_mag
         name="shear"  # Give it a name
     )
 
@@ -45,12 +45,12 @@ def get_shear(data_dir):
 
 
 def get_pressure_tendency(data_dir):
-    data = xr.open_dataset(data_dir + 'era5_surface_pressure_regrid.nc')
+    data = xr.open_dataset(data_dir + 'era5_surface_pressure.nc')
     data = sel_grid(data)
     time_var_all = data.valid_time.values
     data_original = data.sel(valid_time = time_var_all[1:])
     data_prev = data.sel(valid_time = time_var_all[0:-1])
-    data_prev = data_prev.assign({'valid_time': time_var_all})
+    data_prev = data_prev.assign({'valid_time': time_var_all[1:]})
     pressure_tendency = data_original - data_prev
     return pressure_tendency
 
@@ -87,7 +87,7 @@ def get_stream(data_dir):
 
 
 def get_name(variable):
-    if variable == 'total_column_water_vapor':
+    if variable == 'total_column_water_vapour':
         name = 'tcwv'
     elif variable == 'convective_available_potential_energy':
         name = 'cape'
@@ -127,4 +127,6 @@ def get_name(variable):
         name = 'shear925_600'
     elif variable == 'stream':
         name = 'stream'
+    else:
+        raise ValueError('name does not exist')
     return name
